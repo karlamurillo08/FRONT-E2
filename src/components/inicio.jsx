fd89a95ec6890aec34e62357293c22d72b764b53
Alejandra Ponce <PL210665@alumno.udb.edu.sv>
2024-09-26 13:30:34 -0600

    Update Products

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -4,26 +4,69 @@ import Icon from './icon';
 import Ajustes from './ajustes';
 import Usuario from './usuario';
 
-const categories = ['Todo', 'Muebles', 'Ropa', 'Belleza', 'Tecnología']; // Agregamos "Todo" como categoría
+const categories = ['Todo', 'Muebles', 'Calzado', 'Belleza', 'Tecnología']; // Agregamos "Todo" como categoría
 
 const products = [
-
-  { id: '1', name: 'Hamburguesa', price: '$7.50', categoria: 'Comida', image: require('@/../../assets/hamburguesa.jpg') },
-  { id: '2', name: 'Tacos de birria', price: '$8.00', categoria: 'Comida', image: require('@/../../assets/tacos.jpg') },
+  { id: '1', name: 'Smart band', price: '$45.00', categoria: 'Tecnología', image: require('@/../../assets/smartband.webp') },
+  { id: '16', name: 'Ropero 8 puertas', price: '$449.00', categoria: 'Muebles', image: require('@/../../assets/ropero.webp') },
+  { id: '2', name: 'Audífono TWS ', price: '$8.00', categoria: 'Tecnología', image: require('@/../../assets/audifonostwc.jpeg') },
+  { id: '35', name: 'Calzado Sandalia', price: '$39.90', categoria: 'Calzado', image: require('@/../../assets/calzado.webp') },
   { id: '3', name: 'Celular Iphone', price: '$165.00', categoria: 'Tecnología', image: require('@/../../assets/celular.jpg' )},
-  { id: '4', name: 'Reloj rosa', price: '$45.00', categoria: 'Tecnología', image: require('@/../../assets/reloj.webp') },
-  { id: '5', name: 'Reloj negro', price: '$49.00', categoria: 'Tecnología', image: require('@/../../assets/relojnegro.webp') },
+  { id: '54', name: 'Clean Invisible', price: '$7.30', categoria: 'Belleza', image: require('@/../../assets/corrector.webp') },
+  { id: '45', name: 'Zapato Hoka ', price: '$179.90', categoria: 'Calzado', image: require('@/../../assets/hoka.webp') },
+  { id: '17', name: 'Cama Firme ', price: '$410.00', categoria: 'Muebles', image: require('@/../../assets/cama.webp') },
+  { id: '41', name: 'Zapato formal', price: '$59.90', categoria: 'Calzado', image: require('@/../../assets/zapato2.webp') },
+  { id: '4', name: 'Barra de sonido', price: '$219.00', categoria: 'Tecnología', image: require('@/../../assets/barrasonido.jpeg') },
+  { id: '51', name: 'Rubor Polvo', price: '$37.99', categoria: 'Belleza', image: require('@/../../assets/rubor.webp') },
+  { id: '5', name: 'Reloj smartwatch ', price: '$49.00', categoria: 'Tecnología', image: require('@/../../assets/reloj.webp') },
+  { id: '39', name: 'Calzado Sandalia  ', price: '$39.99', categoria: 'Calzado', image: require('@/../../assets/sandaliarojo.webp') },
+  { id: '25', name: 'Ropero 3 puertas', price: '$299.00', categoria: 'Muebles', image: require('@/../../assets/3.webp') },
+  { id: '52', name: 'Oil-Absorbing', price: '$15.99', categoria: 'Belleza', image: require('@/../../assets/piedra.webp') },
   { id: '6', name: 'Celular Samsung', price: '$199.00', categoria: 'Tecnología', image: require('@/../../assets/Celular-Samsung.webp') },
-  { id: '6', name: 'Celular motorola', price: '$105.00', categoria: 'Tecnología', image: require('@/../../assets/motorola.webp') },
-  { id: '7', name: 'Celular Huawei', price: '$115.00', categoria: 'Tecnología', image: require('@/../../assets/huawei.webp') },
-  
-
-
-
-
-
-
-
+  { id: '7', name: 'Celular motorola', price: '$105.00', categoria: 'Tecnología', image: require('@/../../assets/motorola.webp') },
+  { id: '44', name: 'Zapato Nike ', price: '$79.90', categoria: 'Calzado', image: require('@/../../assets/nike.webp') },
+  { id: '59', name: 'Clinique Líquido', price: '$34.99', categoria: 'Belleza', image: require('@/../../assets/desmaquillante.webp') },
+  { id: '29', name: 'Comedor ', price: '$599.90', categoria: 'Muebles', image: require('@/../../assets/comedor.webp') },
+  { id: '56', name: 'Desmaquillador ', price: '$5.99', categoria: 'Belleza', image: require('@/../../assets/Desmaquillador.webp') },
+  { id: '8', name: 'Celular Huawei', price: '$115.00', categoria: 'Tecnología', image: require('@/../../assets/huawei.webp') },
+  { id: '9', name: 'Pantalla Samsung', price: '$876.00', categoria: 'Tecnología', image: require('@/../../assets/pantallasamsung.webp') },
+  { id: '28', name: 'Silla de Oficina', price: '$125.00', categoria: 'Muebles', image: require('@/../../assets/silla.jpeg') },
+  { id: '10', name: 'Pantalla Xiaomi', price: '$876.00', categoria: 'Tecnología', image: require('@/../../assets/pantallaxiaomi.webp') },
+  { id: '11', name: 'Audífonos', price: '$19.90', categoria: 'Tecnología', image: require('@/../../assets/audifonos.webp') },
+  { id: '58', name: 'Clinique Mascara', price: '31.99', categoria: 'Belleza', image: require('@/../../assets/mascara.webp') },
+  { id: '43', name: 'Zapato Adidas ', price: '$79.90', categoria: 'Calzado', image: require('@/../../assets/adidas2.webp') },
+  { id: '12', name: 'Kit de internet', price: '$280.00', categoria: 'Tecnología', image: require('@/../../assets/internet.webp') },
+  { id: '36', name: 'Sandalia casual', price: '$39.99', categoria: 'Calzado', image: require('@/../../assets/sandaliacasual.webp') },
+  { id: '34', name: 'Calzado Mocasin', price: '$45.99', categoria: 'Calzado', image: require('@/../../assets/mocasin2.webp') },
+  { id: '27', name: 'Soporte movible', price: '$29.40', categoria: 'Muebles', image: require('@/../../assets/soporte.webp') },
+  { id: '13', name: 'Impresora l3250 ', price: '$224.00', categoria: 'Tecnología', image: require('@/../../assets/impresora.webp') },
+  { id: '14', name: 'Adaptador RJ45', price: '$45.00', categoria: 'Tecnología', image: require('@/../../assets/adaptador.webp') },
+  { id: '53', name: 'Máscara de pestaña', price: '$12.99', categoria: 'Belleza', image: require('@/../../assets/pestalol.webp') },
+  { id: '30', name: 'Loveseat ', price: '$559.00', categoria: 'Muebles', image: require('@/../../assets/Loveseat.webp') },
+  { id: '15', name: 'Control switch ', price: '$99.90', categoria: 'Tecnología', image: require('@/../../assets/control.webp') },
+  { id: '37', name: 'Sandalia casual', price: '$39.99', categoria: 'Calzado', image: require('@/../../assets/sandaliablanca.webp') },
+  { id: '18', name: 'Rack Dragon', price: '$99.90', categoria: 'Muebles', image: require('@/../../assets/dragon.png') },
+  { id: '19', name: 'Protector', price: '$17.90', categoria: 'Muebles', image: require('@/../../assets/protector.webp') },
+  { id: '60', name: 'Tattoo Studio Brow', price: '$10.73', categoria: 'Belleza', image: require('@/../../assets/Tattoo.webp') },
+  { id: '40', name: 'Zapato de vestir  ', price: '$59.90', categoria: 'Calzado', image: require('@/../../assets/zapato.webp') },
+  { id: '20', name: 'Sofá cama', price: '$869.00', categoria: 'Muebles', image: require('@/../../assets/sofa.webp') },
+  { id: '55', name: 'Blush ', price: '$12.99', categoria: 'Belleza', image: require('@/../../assets/Blush.webp') },
+  { id: '42', name: 'Zapato Adidas ', price: '$79.90', categoria: 'Calzado', image: require('@/../../assets/adidas.webp') },
+  { id: '21', name: 'Gavetero', price: '$189.00', categoria: 'Muebles', image: require('@/../../assets/gavetero.jpeg') },
+  { id: '22', name: 'Kit de mesas ', price: '$172.35', categoria: 'Muebles', image: require('@/../../assets/kit.webp') },
+  { id: '38', name: 'Calzado Mocasin ', price: '$45.90', categoria: 'Calzado', image: require('@/../../assets/mocasinrojo.webp') },
+  { id: '23', name: 'Modulo bajo ', price: '$120.00', categoria: 'Muebles', image: require('@/../../assets/modulo.webp') },
+  { id: '57', name: 'MAC LÁPIZ', price: '$26.99', categoria: 'Belleza', image: require('@/../../assets/mac.webp') },
+  { id: '24', name: 'Cama individual', price: '$209.10', categoria: 'Muebles', image: require('@/../../assets/individual.webp') },
+  { id: '26', name: 'Sofá cama', price: '$599.90', categoria: 'Muebles', image: require('@/../../assets/sofacama.webp') },
+  { id: '31', name: 'Sandalia', price: '39.90', categoria: 'Calzado', image: require('@/../../assets/sandalia.webp') },
+  { id: '32', name: 'Calzado Mocasin', price: '$45.00', categoria: 'Calzado', image: require('@/../../assets/mocasin.webp') },
+  { id: '33', name: 'Calzado Sandalia', price: '$39.99', categoria: 'Calzado', image: require('@/../../assets/sandalia2.webp') },
+  { id: '46', name: 'Polvo', price: '$11.99', categoria: 'Belleza', image: require('@/../../assets/polvo.webp') },
+  { id: '47', name: 'Foundation ', price: '$20.99', categoria: 'Belleza', image: require('@/../../assets/base.webp') },
+  { id: '48', name: 'Eye Booster', price: '$10.50', categoria: 'Belleza', image: require('@/../../assets/delineador.webp') },
+  { id: '49', name: 'Gel Limpiador', price: '$17.25', categoria: 'Belleza', image: require('@/../../assets/gel.webp') },
+  { id: '50', name: 'Super Smooth', price: '$6.99', categoria: 'Belleza', image: require('@/../../assets/lapiz.webp') },
 ];
 
 const Inicio = () => {
@@ -218,6 +261,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#013A63',
+
   },
   categoryScroll: {
     marginBottom: 0, // Reducido para eliminar el espacio extra
@@ -232,15 +277,15 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 20,
     borderWidth: 1,
-    borderColor: '#007bff',
+    borderColor: '#012A4A',
     marginRight: 10, // Añadido para un mejor espaciado entre categorías
   },
   selectedCategoryButton: {
-    backgroundColor: '#007bff',
+    backgroundColor: '#012A4A',
   },
   categoryText: {
     fontSize: 16,
-    color: '#007bff',
+    color: '#2C7DA0',
   },
   selectedCategoryText: {
     color: '#fff',
